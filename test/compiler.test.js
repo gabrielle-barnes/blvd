5392eac44e5b426fa2a0a5b8d14336d505c4d1ce
funmi idowu <fidowu@lion.lmu.edu>
2024-02-16 13:15:54 -0800

    deleting unused code that was commented out

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,16 +1,3 @@
-// import assert from "node:assert/strict"
-// import compile from "../src/compiler.js"
-
-// // Test for valid syntax
-// assert.strictEqual(compile("valid javascript code"), "Syntax is ok");
-
-// // Test for invalid syntax
-// assert.throws(() => {
-//   compile("invalid javascript code");
-// }, /Syntax error/);
-
-// console.log("All tests passed successfully.");
-
 import assert from "node:assert/strict";
 import compile from "../src/compiler.js";
 //import { Program } from "../src/core.js";
