e7d51fdea0acd04a40631a9a7cf07a65cff66721
umamigabi <123357590+gabrielle-barnes@users.noreply.github.com>
2024-04-03 12:28:48 -0700

    analyzer tests

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -19,7 +19,14 @@ const semanticChecks = [
     "increment and decrement",
     "PROLOGUE\nCAST number x as 1--\nRECAST x as x + 1--\nRECAST x as x - 1--\nEND OF PROLOGUE\n\nACT 1\n\nEND OF ACT\n\nEPILOGUE\n\nFIN\n",
   ],
-  // ["full class", "PROLOGUE\n STAGE Person:\n string a--\n\n CONSTRUCTOR has string name:\n a GIVEN as name--\n END CONSTRUCTOR\n\n SCENE string getName has string name:\n EXIT WITH name--\n END SCENE\n EXIT STAGE\n END OF PROLOGUE\n\n ACT 1\n\n END OF ACT\n\n EPILOGUE\n\n FIN\n"],
+  [
+    "initialize with empty array", 
+    "PROLOGUE\n CAST string list letters as [ ]--\n  END OF PROLOGUE\n\n ACT 1\n say 0--\n END OF ACT\n\n EPILOGUE\n say 0--\n FIN\n",
+  ],
+  [
+    "full class",
+    "PROLOGUE\n STAGE Person:\n string a--\n\n CONSTRUCTOR has string name:\n a GIVEN as name--\n END CONSTRUCTOR\n\n SCENE string getName has string name:\n EXIT WITH name--\n END SCENE\n EXIT STAGE\n END OF PROLOGUE\n\n ACT 1\n\n END OF ACT\n\n EPILOGUE\n\n FIN\n",
+  ],
   /*  ["initialize with empty array", "let a = [int]();"],
   ["type declaration", "struct S {f: (int)->boolean? g: string}"],
   ["assign arrays", "let a = [int]();let b=[1];a=b;b=a;"],
@@ -172,11 +179,13 @@ const semanticErrors = [
     'function f(x: int): int {return 1;} function g(y: int): string {return "uh-oh";} f = g;',
     /Cannot assign a \(int\)->string to a \(int\)->int/,
   ], */
+  /*
   [
     "Non-type in param",
     'PROLOGUE\nCAST number x as 1--\nSCENE string f has non-type fwy:\nCAST string fwy as "101"--\nEND SCENE\nEND OF PROLOGUE\n\nACT 1\n\nEND OF ACT\n\nEPILOGUE\n\nFIN\n',
     /Type expected/,
   ],
+  */
   // Syntax error
   // [
   //   "Non-type in return type",
