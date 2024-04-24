17072520d4d838bb2bf88e96aab0afe14c803e77
Ray Toal <rtoal@lmu.edu>
2024-04-24 15:48:22 -0700

    Put compiler tests back

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -19,19 +19,19 @@ describe("The compiler", () => {
     assert(compiled.startsWith("Syntax is ok"));
     done();
   });
-  // it("accepts the analyzed option", (done) => {
-  //   const compiled = compile(sampleProgram, "analyzed");
-  //   assert(compiled instanceof Program);
-  //   done();
-  // });
-  // it("accepts the optimized option", (done) => {
-  //   const compiled = compile(sampleProgram, "optimized");
-  //   assert(compiled instanceof Program);
-  //   done();
-  // });
-  // it("generates js code when given the js option", (done) => {
-  //   const compiled = compile(sampleProgram, "js");
-  //   assert(compiled.startsWith("console.log(0)"));
-  //   done();
-  // });
+  it("accepts the analyzed option", (done) => {
+    const compiled = compile(sampleProgram, "analyzed");
+    assert(compiled.kind === "Program");
+    done();
+  });
+  it("accepts the optimized option", (done) => {
+    const compiled = compile(sampleProgram, "optimized");
+    assert(compiled.kind === "Program");
+    done();
+  });
+  it("generates js code when given the js option", (done) => {
+    const compiled = compile(sampleProgram, "js");
+    assert(compiled.startsWith("console.log(0)"));
+    done();
+  });
 });
